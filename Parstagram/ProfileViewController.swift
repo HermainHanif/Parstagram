1d5c3e11fe265c4e6aa5c602cfc361b2f3476696
Hamzah <hermainhanif@hermains-macbook-air.local>
2019-02-19 01:17:03 -0800

    detached profile tab for later so no errors

--- Parstagram/ProfileViewController.swift
+++ Parstagram/ProfileViewController.swift
@@ -1,41 +1,37 @@
-////
-////  ProfileViewController.swift
-////  Parstagram
-////
-////  Created by Hermain Hanif on 2/18/19.
-////  Copyright © 2019 Hermain Hanif. All rights reserved.
-////
 //
-//import UIKit
-//import AlamofireImage
-//import Parse
+//  ProfileViewController.swift
+//  Parstagram
 //
-//class ProfileViewController: UIViewController {
+//  Created by Hermain Hanif on 2/19/19.
+//  Copyright © 2019 Hermain Hanif. All rights reserved.
 //
-//    @IBOutlet weak var profileTabImageView: UIImageView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        let imageFile = post["image"] as! PFFileObject
-//        let urlString = imageFile.url!
-//        let url = URL(string: urlString)!
-//
-//        profileTabImageView.af_setImage(withURL: url)
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
+
+import UIKit
+import Parse
+
+
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameLabel.text = PFUser.current()?.username
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
